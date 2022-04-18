b3ca35e73ed33a6e101e03f520863570b5f56c2d
vhs <vhs@certi.org.br>
2022-04-17 21:01:02 -0300

    commit map,buffer,range

--- rxjs-teste/src/app/operatos-rxjs/operatos-rxjs.component.ts
+++ rxjs-teste/src/app/operatos-rxjs/operatos-rxjs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { delay, filter, fromEvent, of } from 'rxjs';
+import { buffer, delay, filter, fromEvent, interval, map, of, range } from 'rxjs';
 
 @Component({
   selector: 'app-operatos-rxjs',
@@ -11,18 +11,43 @@ export class OperatosRxjsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    ///operator filter
-    const clicks = fromEvent(document, 'click');
-   // const clicksOnDivs = clicks.pipe(filter(ev => ev.target.tagName === 'DIV'));
-    const clicksOnDivs = of()
-    clicksOnDivs.subscribe(x => console.log(x));
+    this.functionRange();
+  }
 
+  functionDalay = () =>{
     //dalay
+    const clicks = fromEvent(document, 'click');
     const date = new Date('March 15, 2050 12:00:00'); // in the future
     const delayClicks1 = clicks.pipe(delay(date)); // click emitted after that date
     const delayClicks2 = clicks.pipe(delay(1000)); // click emitted after that date
     delayClicks1.subscribe(x => console.log(x));
     delayClicks2.subscribe(x => console.log(x));
+  }
+
+  functionMap = () => {
+    //map
+    const clicks = fromEvent(document, 'click');
+    const positions = clicks.pipe(map(ev => {
+      const e = ev as MouseEvent;
+      return e.clientX
+      
+    }));
+    positions.subscribe(x => console.log(x))
+  }
+
+  functionBuffer = () => {
+    const clicks = fromEvent(document, 'click');
+    const intervalEvents = interval(1000);
+    const buffered = intervalEvents.pipe(buffer(clicks));
+    buffered.subscribe(x => console.log(x))
+  }
+
+  functionRange = () => {
+    const numbers = range(1,3);
+    numbers.subscribe({
+      next: value => {console.log(value)},
+      complete: () => {console.log('Complete!')}
+    })
   }
 
 }
